70ccaf195a677ab7752909f7ca7d30d6e0334dc2
unknown <kev.s.raj@gmail.com>
2020-03-05 20:32:17 -0500

    helpful message

--- src/main/java/frc/robot/DriveBase.java
+++ src/main/java/frc/robot/DriveBase.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,12 +7,63 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import com.ctre.phoenix.motorcontrol.*;
+
+
 /**
  * Add your docs here.
  */
-public class DriveBase {
+public class Drivebase {
+    TalonSRX RF;
+    TalonSRX RB;
+    TalonSRX LF;
+    TalonSRX LB;
+    DifferentialDrive Drive;
+    double navXYaw;
+    SpeedControllerGroup LeftControllerGroup;
+    SpeedControllerGroup RightControllerGroup;
+    
+    public Drivebase(){
+    
+        RF = new TalonSRX(RobotMap.Right_Forward);
+        LF = new TalonSRX(RobotMap.Left_Forward);
+        RB = new TalonSRX(RobotMap.Right_Back);
+        LB = new TalonSRX(RobotMap.Left_Back);
 
+
+
+
+        LeftControllerGroup = new SpeedControllerGroup(RF, RB);
+
+
+
+
+
+
+
+
+        drivebase = new RobotDrive(RF, LF, RB, LB);
+        drivebase.setInvertedMotor(RobotDrive.MotorType.KFrontLeft, RobotMap.FRONT_LEFT_INV);          
+        drivebase.setInvertedMotor(RobotDrive.MotorType.KFrontLeft, RobotMap.FRONT_RIGHT_INV);
+        drivebase.setInvertedMotor(RobotDrive.MotorType.KFrontLeft, RobotMap._RIGHT_BACK_INV);
+        drivebase.setInvertedMotor(RobotDrive.MotorType.KFrontLeft, RobotMap.BACK_LEFT_INV);
+        drivebase.setSafetyEnabled(true);
+        drivebase.setInvertedMotor(RobotDrive)
+        
+    }
+    public void drive(double left, double right){
+        drivebase.tankDrive(left, right);
+    }
+    public void zeroEncoders(){
+        
+    }
 
 
     
 }
+
